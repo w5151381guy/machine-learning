89ae9b2043e60356bf9da606ad8fe47854ddf977
macbook <w5151381guy@gmail.com>
2017-04-15 14:26:54 +0800

    finish oneVsAll

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -49,14 +49,14 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
+%set initial theta
+initial_theta = zeros(n + 1, 1);
 
+%set options for fminunc
+options = optimset('GradObj','on','MaxIter',50);
 
-
-
-
-
-
-
+for i = 1:num_labels,
+    all_theta(i,:) = fmincg(@(t)(lrCostFunction(t,X,(y == i),lambda)),initial_theta,options);
 
 
 
